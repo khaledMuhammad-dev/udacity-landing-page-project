dda82dd971e36c9d048be46fbc3f4c22ac5a98d0
Khaled Muhammad <khaledmuhammad.dev@gmail.com>
2022-03-10 11:57:43 +0200

    refactor: optimizing performance and add comment to sections

--- js/app.js
+++ js/app.js
@@ -1,137 +1,198 @@
 
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
 
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
 
-const log = console.log;
+/**
+ * Define Global Variables
+ * 
+*/
+    const log = console.log;
+    const sections = querySelector("section[data-nav]", "all");
+    const ul = querySelector("#navbar__list");
+    const activeOffset = 80; //px
 
-const sections = document.querySelectorAll("section[data-nav]");
-const ul = document.querySelector("#navbar__list");
+    // nav menu controls
+    const burgerbtn = querySelector(".burger__menu");
+    const header = querySelector(".page__header");
+    let expand = false;
+    let timeoutRef = -1;
 
-appendNavSections()
-document.addEventListener("scroll", handleActiveSection);
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+    function querySelector(selector, invoke="") {
 
-function appendNavSections() {
-
-    const tempContainer = document.createDocumentFragment();
-
-    sections.forEach(
-        section => {
-            const { nav } = section.dataset;
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-
-            a.setAttribute("href", "#");
-            a.classList.add("menu__link");
-            a.textContent = nav;
-            li.appendChild(a);
-    
-            tempContainer.appendChild(li);
+        if(invoke === "all") {
+            return document.querySelectorAll(selector);
         }
-    );
+        
+        return document.querySelector(selector);
+    }
 
-    ul.appendChild(tempContainer);
-}
+    // detect the menu height and apply the total height for animation purpose
+    // set "show" class to display menu
+    function openMenu() {
+        ul.setAttribute("class", "show"); // display flex
 
+        const menuHeight = ul.offsetHeight;
+        header.style.height = `${ menuHeight + 52 }px`;
+    }
 
- const lis = document.querySelectorAll("li");
+    // remove the style for animation purpose
+    // remove the show class to close the menu
+    function closeMenu() {
+        header.removeAttribute("style");
+        timeoutRef = setTimeout(() => ul.removeAttribute("class"), 300)
+    }
 
- function handleActiveSection() {
-     sections.forEach(( section, i ) => {
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+    // build the nav
+    function appendNavSections() {
+
+        const tempContainer = document.createDocumentFragment();
+        
+        sections.forEach(
+            section => {
+                const li = document.createElement("li");
+                li.innerHTML = `<a href="#" class="menu__link">${section.dataset.nav}</a>`;
+                
+                tempContainer.append(li);
+            }
+        );
+
+        ul.appendChild(tempContainer);
+    }
+
+    // Add class 'active' to section when near top of viewport
+    function handleActiveSection() {
+        // get list items
+        const listItems = querySelector("li", "all");
+
+        sections.forEach(( section, i ) => {
         const { top, bottom } = section.getBoundingClientRect();
         const isActive = section.classList.contains("active");
- 
-        if ( top <= 5 && bottom > 5 ) {
-            if( !isActive ){
+
+        if ( top <= activeOffset && bottom > activeOffset ) {
+            if ( !isActive ) {
                 section.classList.add("active");
-                lis[i].classList.add("active");
-            };
+                listItems[i].classList.add("active");
+            }
             
         } else {
-             
+                
             if( isActive ) {
                 section.classList.remove("active");
-                lis[i].classList.remove("active");
- 
+                listItems[i].classList.remove("active");
             }
         }
     })
-}
-
-
-ul.addEventListener("click", (e) => {
-    const { target } = e;
-    e.preventDefault();
-
-    if( !target.classList.contains("menu__link") ) {
-        return;
     }
 
-    const id = target.textContent
-               .toLowerCase()
-               .replace(/\s/, "");
-               
-    const section = document.querySelector(`#${ id }`);
-    const top = section.offsetTop;
+    // Scroll to anchor ID using scrollTO event
+    function scrollToSection(e) {
+        const { target } = e;
+        e.preventDefault();
 
-    window.scrollTo({
-        top,
-        left: 0,
-        behavior: 'smooth',
-    })
-})
+        if( !target.classList.contains("menu__link") ) {
+            return;
+        }
 
+        const id = target.textContent
+                .toLowerCase()
+                .replace(/\s/, "");
+                
+        const section = querySelector(`#${ id }`);
+        const top = section.offsetTop;
 
-const burgerbtn = document.querySelector(".burger__menu");
-const header = document.querySelector(".page__header");
-let expand = false;
-let timeoutRef = -1;
+        window.scrollTo({
+            top: top - activeOffset,
+            left: 0,
+            behavior: 'smooth',
+        });
+    };
 
+    // toggle menu
+    function toggleMenu() {
+        expand = !expand;
+
+        if(!expand) {
+            closeMenu();
+            return;
+        }
+
+        if (timeoutRef > -1) {
+            clearTimeout(timeoutRef);
+        }
+
+        openMenu();
+    }
+
+    // re-intiate the toggle menu states on resizeing
+    function reInit () {
+        if (window.innerWidth > 786) {
+            expand = false;
+
+            ul.removeAttribute("class");
+            header.removeAttribute("style");
+        }
+    }
+
+    // close the navifarion menu on page click
+    function closeMenuOnpageClick({ target } ) {
+        if(burgerbtn.contains( target )){ 
+            return;
+        }
+
+        if(window.innerWidth <= 786 && expand){ 
+
+            toggleMenu();
+        }
+    }
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+appendNavSections();
+
+// Scroll to section on link click
+ul.addEventListener("click", scrollToSection);
+
+// Set sections as active
+document.addEventListener("scroll", handleActiveSection);
+
+// toggle the menu on burger menu button clicked
 burgerbtn.addEventListener("click", toggleMenu);
+
+// reintiate the menu state if the width greater than 786px
 window.addEventListener("resize", reInit);
 
-document.addEventListener( "click", function({ target } ) {
-    if(burgerbtn.contains( target )){ 
-        return;
-    }
-
-    if(window.innerWidth <= 786 && expand){ 
-
-        toggleMenu()
-    }
-    
-})
-
-function openMenu() {
-    ul.setAttribute("class", "show");
-
-    const menuHeight = ul.offsetHeight;
-    header.style.height = `${ menuHeight + 52 }px`;
-}
-
-function closeMenu() {
-    header.removeAttribute("style");
-    timeoutRef = setTimeout(() => ul.removeAttribute("class"), 300)
-}
-
-function toggleMenu() {
-    expand = !expand;
-
-    if(!expand) {
-        closeMenu()
-        return;
-    }
-
-    if (timeoutRef > -1) {
-        clearTimeout(timeoutRef);
-    }
-
-    openMenu()
-}
-
-function reInit () {
-    if (window.innerWidth > 786) {
-        expand = false;
-        ul.removeAttribute("class");
-        header.removeAttribute("style");
-    }
-    
-}
+// close the menu on page click
+document.addEventListener( "click", closeMenuOnpageClick);
